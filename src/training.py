f77bb02f70d8f2ef0eac3c8a3df4af2c34d8ef9e
gaurav98094 <kandelgaurav3@gmail.com>
2021-11-08 20:03:18 +0545

    added model save

--- src/training.py
+++ src/training.py
@@ -2,41 +2,42 @@ from yaml.parser import Parser
 from utils.common import read_config
 
 from utils.data_mgmt import get_data
-from utils.model import create_model
-
+from utils.model import create_model, save_model
+import os
 
 import argparse
 
+
 def training(config_path):
     config = read_config(config_path)
-    
+
     valid_data_size = config["params"]["validation_datasize"]
-    (X_train,y_train),(X_val,y_val),(X_test,y_test) = get_data(valid_data_size)
+    (X_train, y_train), (X_val, y_val), (X_test, y_test) = get_data(valid_data_size)
 
     LOSS_FUNC = config["params"]["loss_function"]
     OPTIMIZER = config["params"]["optimizer"]
     metrics = [config["params"]["metrics"]]
     input_shape = config["params"]["input_shape"]
 
-
-    model = create_model(input_shape,LOSS_FUNC,OPTIMIZER,metrics)
-
+    model = create_model(input_shape, LOSS_FUNC, OPTIMIZER, metrics)
 
     EPOCHS = config["params"]["epochs"]
-    VALIDATION = (X_val,y_val)
+    VALIDATION = (X_val, y_val)
 
-    history = model.fit(X_train,y_train,epochs=EPOCHS,validation_data=VALIDATION)
+    history = model.fit(X_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
 
+    model_name = config["artifacts"]["model_name"]
+    model_dir = config["artifacts"]["model_dir"]
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok=True)
 
-    return model
- 
-
-
+    save_model(model, model_name, model_dir_path)
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument("--config","-c",default="config.yaml")
+    args.add_argument("--config", "-c", default="config.yaml")
 
     parsed_args = args.parse_args()
 
--- src/utils/model.py
+++ src/utils/model.py
@@ -1,19 +1,29 @@
 import tensorflow as tf
+import time
+import os
 
-def create_model(input_shape,LOSS_FUNC,OPTIMIZER,metrics):
+def create_model(input_shape, LOSS_FUNC, OPTIMIZER, metrics):
     LAYERS = [
-          tf.keras.layers.Flatten(input_shape=input_shape,name="input"),
-          tf.keras.layers.Dense(300,activation='sigmoid',name="hidden1"),
-          tf.keras.layers.Dense(100,activation='sigmoid',name="hidden2"),
-          tf.keras.layers.Dense(10,activation='softmax',name="output")
+        tf.keras.layers.Flatten(input_shape=input_shape, name="input"),
+        tf.keras.layers.Dense(300, activation='sigmoid', name="hidden1"),
+        tf.keras.layers.Dense(100, activation='sigmoid', name="hidden2"),
+        tf.keras.layers.Dense(10, activation='softmax', name="output")
     ]
     model = tf.keras.models.Sequential(LAYERS)
 
     print(model.summary())
 
+    model.compile(loss=LOSS_FUNC, optimizer=OPTIMIZER, metrics=metrics)
 
-    model.compile(loss=LOSS_FUNC,optimizer=OPTIMIZER,metrics=metrics)
-
- 
     return model
-     
+
+
+def get_unique_filename(filename):
+    unique_filename = time.strftime(f"{filename}_%Y-%m-%d_T_%H_%M_%S.h5")
+    return unique_filename
+
+
+def save_model(model, model_name,model_dr):
+    unique_filename = get_unique_filename(model_name)
+    path = os.path.join(model_dr,unique_filename)
+    model.save(path)
